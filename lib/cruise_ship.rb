bb4e84152de668c26fd5d4f0e92b98e472a321b4
Judy McGoldrick <jjeanius@verizon.net>
2017-12-21 04:52:02 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,8 +1,8 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
 
@@ -14,11 +14,11 @@ passengers.each do |suite, name|
   end
 end
 
-winner 
-  
+winner
 
 
 
-  
+
+
   # add the code snippet here!
 end
